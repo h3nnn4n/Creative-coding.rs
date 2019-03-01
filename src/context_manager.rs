abf699340661f2327146dadcbb860cd862ff73ff
h3nnn4n <uber.renan@gmail.com>
2019-02-28 23:48:38 -0300

    Move cairo into a wrapper

---
+++ src/context_manager.rs
@@ -0,0 +1,83 @@
+use cairo::{Context, Format, ImageSurface};
+use std::f64::consts::PI;
+use std::fs::File;
+
+#[derive(Debug)]
+pub struct ContextManager {
+    context: Context,
+    surface: ImageSurface,
+    filename: String,
+    file: Option<File>,
+    width: i32,
+    height: i32,
+}
+
+impl ContextManager {
+    pub fn init(width: i32, height: i32) -> ContextManager {
+        let surface = ImageSurface::create(Format::ARgb32, width, height)
+            .expect("Couldn’t create a surface!");
+        let context = Context::new(&surface);
+
+        ContextManager {
+            context: context,
+            surface: surface,
+            filename: "output.png".to_string(),
+            file: None,
+            width: width,
+            height: height,
+        }
+    }
+
+    pub fn fill(&mut self) -> &mut Self {
+        self.context.fill();
+
+        self
+    }
+
+    pub fn stroke(&mut self) -> &mut Self {
+        self.context.stroke();
+
+        self
+    }
+
+    pub fn circle(&mut self, x: f32, y: f32, r: f32) -> &mut Self {
+        self.context
+            .arc(x as f64, y as f64, r as f64, 0.0, PI * 2.0);
+
+        self
+    }
+
+    pub fn move_to(&mut self, x: f32, y: f32) -> &mut Self {
+        self.context.move_to(x as f64, y as f64);
+
+        self
+    }
+
+    pub fn line_to(&mut self, x: f32, y: f32) -> &mut Self {
+        self.context.line_to(x as f64, y as f64);
+
+        self
+    }
+
+    pub fn set_source_rgb(&mut self, r: f32, g: f32, b: f32) -> &mut Self {
+        self.context.set_source_rgb(r as f64, g as f64, b as f64);
+
+        self
+    }
+
+    pub fn set_filename(&mut self, filename: String) -> &mut Self {
+        self.filename = filename;
+
+        self
+    }
+
+    pub fn save(&mut self) -> &mut Self {
+        let mut file = File::create(&self.filename).expect("Couldn'n create output file!");
+
+        self.surface
+            .write_to_png(&mut file)
+            .expect("Couldnt not write to png");
+
+        self
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,20 @@
 extern crate coord;
 extern crate cairo;
 extern crate rand;
-use cairo::{Context, Format, ImageSurface};
 use coord::math::VecFloat;
-use std::f64::consts::PI;
-use std::fs::File;
 
+mod context_manager;
 mod particle;
 
 fn main() {
-    let mut file = File::create("output.png").expect("Couldn’t create file.");
-
     let n_particles = 50;
     let screen_x = 800;
     let screen_y = 800;
 
     let mut particles = Vec::new();
+
+    let mut context = context_manager::ContextManager::init(screen_x, screen_y);
+    context.set_filename("output.png".to_string());
 
     for _ in 0..n_particles {
         let mut p = particle::Particle::init();
@@ -29,11 +28,6 @@ fn main() {
         particles.push(p);
     }
 
-    let surface =
-        ImageSurface::create(Format::ARgb32, 600, 600).expect("Couldn’t create a surface!");
-
-    let context = Context::new(&surface);
-
     context.set_source_rgb(1.0, 1.0, 1.0);
     context.fill();
 
@@ -44,19 +38,17 @@ fn main() {
         let y = particle.position.y;
         let r = particle.radius;
 
-        context.arc(x as f64, y as f64, r as f64, 0.0, PI * 2.0);
+        context.circle(x, y, r);
 
         context.fill();
     }
 
-    draw_lines(particles, context);
+    draw_lines(particles, &mut context);
 
-    surface
-        .write_to_png(&mut file)
-        .expect("Couldnt not write to png");
+    context.save();
 }
 
-fn draw_lines(particles: Vec<particle::Particle>, context: Context) {
+fn draw_lines(particles: Vec<particle::Particle>, context: &mut context_manager::ContextManager) {
     let n_particles = particles.len();
     let cutover_distance = 150.0;
 
@@ -68,9 +60,10 @@ fn draw_lines(particles: Vec<particle::Particle>, context: Context) {
             let p2 = &particles[j];
 
             if (p1.position - p2.position).length() < cutover_distance {
-                context.move_to(p1.position.x as f64, p1.position.y as f64);
-                context.line_to(p2.position.x as f64, p2.position.y as f64);
-                context.stroke();
+                context
+                    .move_to(p1.position.x, p1.position.y)
+                    .line_to(p2.position.x, p2.position.y)
+                    .stroke();
             }
         }
     }
